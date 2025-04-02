805af597c5feb17b78e367ec94a2c4d9c87b2255
Lorry139 <randrianirina.lorry_mael@courrier.uqam.ca>
2025-04-02 08:54:37 -0400

    Update map-layers.js

--- lab11/map-layers.js
+++ lab11/map-layers.js
@@ -1,63 +1,61 @@
-map.addSource('commerces_source', {
-    type: 'geojson',
-    data: 'https://donnees.montreal.ca/dataset/c1d65779-d3cb-44e8-af0a-b9f2c5f7766d/resource/ece728c7-6f2d-4a51-a36d-21cd70e0ddc7/download/businesses.geojson'
-  });
-  
-  map.addLayer({
-    id: 'commerces',
-    type: 'circle',
-    source: 'commerces_source',
-    paint: {
-      // Rayon variable selon le type
-      'circle-radius': [
-        'match',
-        ['get', 'type'],
-        'Épicerie', 5,
-        'Pâtisserie/Boulangerie', 7,
-        'Distributrice automatique', 4,
-        'Pharmacie', 6,
-        'Restaurant', 5,
-        3
-      ],
-      // Couleur variable selon le type
-      'circle-color': [
-        'match',
-        ['get', 'type'],
-        'Épicerie', 'orange',
-        'Pâtisserie/Boulangerie', 'yellow',
-        'Distributrice automatique', 'blue',
-        'Pharmacie', 'green',
-        'Restaurant', 'purple',
-        'grey'
-      ],
-      'circle-stroke-color': '#fff',
-      'circle-stroke-width': 1
-    },
-    
-  });
+var commercesSource = {
+  type: 'geojson',
+  data: 'https://donnees.montreal.ca/dataset/c1d65779-d3cb-44e8-af0a-b9f2c5f7766d/resource/ece728c7-6f2d-4a51-a36d-21cd70e0ddc7/download/businesses.geojson'
+};
 
-map.addSource('arrondissements_source', {
-    type: 'geojson', // Type de source de données
-    data: 'https://donnees.montreal.ca/dataset/70f66e93-89b2-442e-83eb-5d82f7b78c38/resource/c0a93a76-1eb8-41bb-83f0-e1705ae0f4e9/download/arrondissements.geojson' // URL pgFeatureServ GeoJSON ! Attention il faut bien inclure la méthode qui fait la requete sans limite d'items de données
-});
+var commercesLayer = {
+  id: 'commerces',
+  type: 'circle',
+  source: 'commerces_source',
+  paint: {
+    'circle-radius': [
+      'match',
+      ['get', 'type'],
+      'Épicerie', 5,
+      'Pâtisserie/Boulangerie', 7,
+      'Distributrice automatique', 4,
+      'Pharmacie', 6,
+      'Restaurant', 5,
+      3
+    ],
+    'circle-color': [
+      'match',
+      ['get', 'type'],
+      'Épicerie', 'orange',
+      'Pâtisserie/Boulangerie', 'yellow',
+      'Distributrice automatique', 'blue',
+      'Pharmacie', 'green',
+      'Restaurant', 'purple',
+      'grey'
+    ],
+    'circle-stroke-color': '#fff',
+    'circle-stroke-width': 1
+  },
+  filter: ['==', ['get', 'statut'], 'Ouvert']
+};
 
-map.addLayer({
-    id: 'arrondissements',
-    type: 'fill',
-    source: 'arrondissements_source',
-    paint: {
-      'fill-outline-color': 'black',
-      'fill-color': '#cccccc',
-      'fill-opacity': 0.3
-    }
-  });
+var arrondissementsSource = {
+  type: 'geojson',
+  data: 'https://donnees.montreal.ca/dataset/9797a946-9da8-41ec-8815-f6b276dec7e9/resource/e18bfd07-edc8-4ce8-8a5a-3b617662a794/download/limites-administratives-agglomeration.geojson'
+};
+ 
+var arrondissementsLayer = {
+  id: 'arrondissements',
+  type: 'fill',
+  source: 'arrondissements_source',
+  paint: {
+    'fill-color': '#ccc',
+    'fill-opacity': 0.3,
+    'fill-outline-color': '#000'
+  }
+};
 
-  map.addLayer({
+var arrondissementsLabelsLayer = {
     id: 'arrondissements-labels',
     type: 'symbol',
-    source: 'arrondissements_source',
+    source: 'arrondissements-source',
     layout: {
-      'text-field': ['get', 'nom'],
+      'text-field': ['get', 'NOM'],
       'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
       'text-size': 14,
       'text-anchor': 'center'
@@ -66,6 +64,7 @@ map.addLayer({
       'text-color': '#111',
       'text-halo-color': '#fff',
       'text-halo-width': 1.5
-    }
-  });
+    },
+    maxzoom: 13
+  };
   
